a8ec5348ef88f61a9a6397f1566efa8ac4cf8c36
Rhys Camm <rhysjc@gmail.com>
2022-08-24 16:07:02 +0100

    Change Stryde link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ export const Projects: React.FC = () => {
   return (
     <div>
       <h2 className='font-semibold text-3xl md:text-4xl text-zinc-800 dark:text-zinc-100 mt-8'>A few side projects</h2>
-      <Project name="Stryde" link="https://apps.apple.com/us/app/stryde/id1640276342">Workout planner built with React Native</Project>
+      <Project name="Stryde" link="https://apps.apple.com/app/stryde/id1640276342">Workout planner built with React Native</Project>
       <Project name="This website" link="https://github.com/Rhysjc/rhysc.me">Gatsby static site deployed to AWS using CloudFormation</Project>
     </div>
   );
