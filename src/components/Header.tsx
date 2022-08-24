536034484a1c612818656dc43dcf1e2695ce6e29
Rhys Camm <rhysjc@gmail.com>
2022-08-24 10:43:23 +0100

    Change email link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
   return (
     <header className="w-full">
       <div className="container m-auto flex flex-row justify-end items-center py-4">
-        <a href="mailto:hi@rhysc.me" target="_blank" rel="noreferrer" className="mr-4"><Email /></a>
+        <a href="mailto:rhysjc@gmail.com" target="_blank" rel="noreferrer" className="mr-4"><Email /></a>
         <a href="https://github.com/Rhysjc" target="_blank" rel="noreferrer" className="mr-4"><Github /></a>
         <a href="https://twitter.com/rhysjcamm" target="_blank" rel="noreferrer" className="mr-4"><Twitter /></a>
         <DarkModeToggle />
