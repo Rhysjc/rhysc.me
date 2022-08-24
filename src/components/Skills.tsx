730821c8c78d7c0639d19a430e9acd8b01be311f
Rhys Camm <rhysjc@gmail.com>
2022-08-24 09:52:38 +0100

    Favicon

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,8 +11,8 @@ export const Skills: React.FC = () => {
 
   return (
     <div>
-      <h2 className='font-semibold text-3xl lg:text-4xl text-zinc-800 dark:text-zinc-100 mt-4'>Some of the tech I've worked with</h2>
-      <input type="text" placeholder='Filter' className='bg-transparent outline-none text-zinc-800 dark:text-zinc-100 mb-1 w-full placeholder:text-zinc-400' value={filterText} onChange={e => setFilterText(e.target.value)} />
+      <h2 className='font-semibold text-3xl lg:text-4xl text-zinc-800 dark:text-zinc-100 mt-8'>Some of the tech I've worked with</h2>
+      <input type="text" placeholder='Filter' className='bg-transparent outline-none text-zinc-800 dark:text-zinc-100 mb-1 w-full placeholder:text-zinc-40 caret-purple-600' value={filterText} onChange={e => setFilterText(e.target.value)} />
       <div>{filteredSkills.map(skill => <SkillTag key={skill}>{skill}</SkillTag>)}</div>
     </div>
   );
