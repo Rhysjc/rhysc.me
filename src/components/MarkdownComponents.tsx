cdb3df5b9cc760fae1c718b588ef84f361d5b918
Rhys Camm <rhysjc@gmail.com>
2022-08-26 08:44:25 +0100

    Styling tweaks

--- src/components/MarkdownComponents.tsx
+++ src/components/MarkdownComponents.tsx
@@ -1,9 +1,9 @@
 import { MDXProviderComponentsProp } from '@mdx-js/react';
 import React from 'react';
 
-const h2: React.FC<React.PropsWithChildren> = ({ children }) => <h2 className='font-semibold text-3xl md:text-4xl text-zinc-800 dark:text-zinc-100 mt-8 first-of-type:mt-0'>{children}</h2>;
+const h2: React.FC<React.PropsWithChildren> = ({ children }) => <h2 className='font-semibold text-3xl md:text-4xl text-zinc-800 dark:text-zinc-100 mt-8 mb-4 first-of-type:mt-0'>{children}</h2>;
 
-const p: React.FC<React.PropsWithChildren> = ({ children }) => <p className='text-lg text-zinc-800 dark:text-zinc-100 mb-2 only:mb-0'>{children}</p>;
+const p: React.FC<React.PropsWithChildren> = ({ children }) => <p className='text-lg text-zinc-800 dark:text-zinc-100 mb-6 only:mb-0'>{children}</p>;
 
 const blockquote: React.FC<React.PropsWithChildren> = ({ children }) => <blockquote className='pl-4 mt-2 py-4 border-l-2 border-zinc-300 dark:border-zinc-100'>{children}</blockquote>;
 
