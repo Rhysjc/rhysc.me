70341996a3404e83bebf650bff21e852863aacbe
Rhys Camm <rhysjc@gmail.com>
2022-08-26 16:15:55 +0100

    Add sitemaps

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,26 @@
 import type { GatsbyConfig } from 'gatsby';
 
+interface SitemapQuery {
+  allSitePage: {
+    nodes: Array<{
+      path: string
+    }>
+  }
+  allMdx: {
+    nodes: Array<{
+      frontmatter: {
+        slug: string
+        date: string
+      }
+    }>
+  }
+};
+
 const config: GatsbyConfig = {
   graphqlTypegen: true,
+  siteMetadata: {
+    siteUrl: 'https://rhysc.me',
+  },
   plugins: [
     'gatsby-plugin-postcss',
     {
@@ -47,6 +66,48 @@ const config: GatsbyConfig = {
         enableOnDevMode: false,
         pageViews: 'all',
         trackPageViewsAs: 'Page view',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/',
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allMdx {
+            nodes {
+              frontmatter {
+                slug
+                date(formatString: "YYYY-MM-DD")
+              }
+            }
+          }
+        }        
+        `,
+        resolvePages: (data: SitemapQuery) => {
+          const blogPostNodes: Record<string, { lastmod: string }> = data.allMdx.nodes.reduce((currentNodes, node) => {
+            return {
+              ...currentNodes,
+              [`/blog/${node.frontmatter.slug}/`]: { lastmod: node.frontmatter.date },
+            };
+          }, {});
+
+          return data.allSitePage.nodes.map((node) => ({ ...node, ...blogPostNodes[node.path] }));
+        },
+        serialize: ({ path, lastmod }: { path: string, lastmod?: string }) => ({
+          url: path,
+          lastmod,
+        }),
       },
     },
   ],
