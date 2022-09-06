6ae63bd1297718b49ff313246b77a18f5cb62e4c
Rhys Camm <rhysjc@gmail.com>
2022-09-06 10:35:34 +0100

    Add product hunt badge

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ export const Projects: React.FC = () => {
   return (
     <div>
       <Heading>A few side projects</Heading>
-      <Project name="Stryde" link="https://apps.apple.com/app/stryde/id1640276342">Workout planner built with React Native</Project>
+      <Project name="Stryde" link="https://apps.apple.com/app/stryde/id1640276342" extras={<a href="https://www.producthunt.com/products/stryde?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-stryde" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=357980&theme=dark&period=daily" alt="Stryde - A&#0032;super&#0032;simple&#0032;&#0038;&#0032;flexible&#0032;workout&#0032;planner | Product Hunt" style={{ width: '250px', height: '55px' }} width="250" height="54" /></a>}>Workout planner built with React Native</Project>
       <Project name="This website" link="https://github.com/Rhysjc/rhysc.me">Gatsby static site deployed to AWS using CloudFormation</Project>
     </div>
   );
